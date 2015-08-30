5f60cc0202a8292e3a99270895acef060d3afafa
Scott R. Santos <santos@auburn.edu>
2015-08-29 23:05:10 -0400

    Added rofi_todo.sh script

---
+++ rofi_todo.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Write/remove a task to do later.
+
+FILE=~/.todo
+HEIGHT=$(cat $FILE | wc -l)
+
+ACTION="cat $FILE | rofi -dmenu -lines 5 -p 'Add/delete a task: '"
+CMD=$(eval $ACTION)
+while [ -n "$CMD" ]; do
+	grep -q "^$CMD" $FILE
+	if [ $? = 0 ]; then
+		grep -v "^$CMD" $FILE > /tmp/todo
+		mv /tmp/todo $FILE
+        HEIGHT=$(($HEIGHT-1))
+        ACTION="cat $FILE | rofi -dmenu -lines 5 -p 'Add/delete a task: '"
+	else
+		echo "$CMD" >> $FILE
+        HEIGHT=$(($HEIGHT+1))
+        ACTION="cat $FILE | rofi -dmenu -lines 5 -p 'Add/delete a task: '"
+	fi
+
+	CMD=$(eval $ACTION)
+
+done
+exit 0
