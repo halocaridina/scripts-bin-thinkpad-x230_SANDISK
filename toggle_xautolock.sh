29d91f76e29f36196838ff41a6cedc0ad624dff6
Scott R. Santos <santos@auburn.edu>
2016-01-28 20:56:09 -0500

    Changed tags to Toggled

--- toggle_xautolock.sh
+++ toggle_xautolock.sh
@@ -5,10 +5,10 @@ TOGGLE="/tmp/screen_lock_toggled_off.pid"
 if [ ! -e $TOGGLE ]; then
     touch $TOGGLE && echo "1" > /tmp/screen_lock_toggled_off.pid
     /usr/bin/pkill xautolock
-    notify-send 'Auto-Suspend' 'Disabled' -i /usr/share/icons/gnome-colors-common/scalable/status/status_unlocked.svg
+    notify-send 'Auto-Suspend' 'Toggled' -i /usr/share/icons/gnome-colors-common/scalable/status/status_unlocked.svg
 else
     rm -f $TOGGLE
     /usr/bin/xautolock -time 30 -locker "systemctl suspend" -detectsleep &
-    notify-send 'Auto-Suspend' 'Enabled' -i /usr/share/icons/gnome-colors-common/scalable/status/status_lock.svg
+    notify-send 'Auto-Suspend' 'Toggled' -i /usr/share/icons/gnome-colors-common/scalable/status/status_lock.svg
 fi
 
