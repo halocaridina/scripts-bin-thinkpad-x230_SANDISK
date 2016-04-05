f7af7bf662b71fb8b182fc99b06eea1406075f2e
Scott R. Santos <santos@auburn.edu>
2016-04-05 18:10:49 -0400

    Added -4 option to ping

--- update_arch_notification.sh
+++ update_arch_notification.sh
@@ -3,7 +3,7 @@
 UPDATES="`/usr/bin/checkupdates`"
 NUM_UPDATES="`/usr/bin/checkupdates | wc -l`"
 
-if ! [ "`ping -c 1 google.com`" ]; then
+if ! [ "`ping -4 -c 1 google.com`" ]; then
     notify-send -u critical "Currently offline." "Will check for Archlinux updates again in 2 hrs."
 elif [ "$NUM_UPDATES" == 0 ]; then
     notify-send -u low -i /usr/share/icons/gnome-colors-common/scalable/apps/system-software-update.svg 'No updates for Archlinux.'
--- update_aur_notification.sh
+++ update_aur_notification.sh
@@ -6,7 +6,7 @@ export XAUTHORITY=/home/srsantos/.Xauthority
 UPDATES="`pacaur -k | awk '{print $3}' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"`"
 NUM_UPDATES="`pacaur -k | awk '{print $3}' | wc -l`"
 
-if ! [ "`ping -c 1 google.com`" ]; then
+if ! [ "`ping -4 -c 1 google.com`" ]; then
     notify-send -u critical "Currently offline." "Will check for AUR updates again in 2 hrs."
 elif [ "$NUM_UPDATES" == 0 ]; then
     notify-send -u low -i /usr/share/icons/gnome-colors-common/scalable/apps/system-software-update.svg 'No updates from AUR.'
