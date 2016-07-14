aba53c08fe26fdc9e2b78b1d606e533ec8928e7a
Scott R. Santos <santos@auburn.edu>
2016-07-14 13:36:58 -0400

    Added notify-send to the script

--- brightness_notification.sh
+++ brightness_notification.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-/usr/bin/aplay /home/srsantos/.i3/sounds/Click1.wav
+/usr/bin/notify-send -i /usr/share/icons/Adwaita/48x48/status/battery-caution-charging.png 'Changing Power States' && sleep 10 &
+/usr/bin/aplay /home/srsantos/.i3/sounds/Click1.wav > /dev/null 2>&1
