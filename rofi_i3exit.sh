de3adcfbc68dd2d3b5376a6fa4c6a51f838ac1a2
Scott R. Santos <santos@auburn.edu>
2016-07-18 09:30:56 -0400

    Streamlined rofi flags since tweaked config by Xresources

--- rofi_i3exit.sh
+++ rofi_i3exit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-res=$(rofi -color-enabled "true" -color-window "#222222,#396EB4" -color-urgent "#222222,#EF2929,#222222,#396EB4,#FFFFFF" -color-normal "#222222,#FFFFFF,#222222,#396EB4,#FFFFFF" -dmenu -lines 5 -p "What do you want to do? " -i -u 2-4 < ~/.i3/rofi-i3exit.opts)
+res=$(rofi -dmenu -lines 5 -u 2-4 -p "What do you want to do? " -input < ~/.i3/rofi-i3exit.opts)
 
 if [[ $res = "LOCK SCREEN" ]]; then
     xset dpms force off
