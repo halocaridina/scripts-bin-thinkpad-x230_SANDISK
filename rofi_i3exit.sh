f6d742e8c90c2b73c5a69bdf80e6caa7c81193d3
Scott R. Santos <santos@auburn.edu>
2016-07-31 16:23:40 -0400

    Changed order of options

--- rofi_i3exit.sh
+++ rofi_i3exit.sh
@@ -5,14 +5,14 @@ res=$(rofi -dmenu -lines 6 -u 3-5 -p "What do you want to do? " -input < ~/.i3/r
 if [[ $res = "LOCK SCREEN" ]]; then
     xset dpms force off
 fi
+if [[ $res = "LOGOUT TO TTY" ]]; then
+    pkill -f xss-lock && pkill -f bash && pkill -f gnome-keyring-d && rm -f /tmp/screen_lock_toggled_off.lock /tmp/openconnect_vpn.lock && pkill -f sleep && i3-msg exit
+fi
 if [[ $res = "SUSPEND TO RAM" ]]; then
     systemctl suspend
 fi
 if [[ $res = "HIBERNATE TO DISK" ]]; then
     systemctl hibernate
-fi
-if [[ $res = "LOGOUT TO TTY" ]]; then
-    pkill -f xss-lock && pkill -f bash && pkill -f gnome-keyring-d && rm -f /tmp/screen_lock_toggled_off.lock /tmp/openconnect_vpn.lock && pkill -f sleep && i3-msg exit
 fi
 if [[ $res = "REBOOT" ]]; then
     systemctl reboot
