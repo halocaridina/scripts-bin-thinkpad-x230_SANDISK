9ff0ddf5c0c8393e4e8a0b6a5771686c200d90e3
Scott R. Santos <santos@auburn.edu>
2016-08-18 06:30:38 -0500

    Changed logic order of script to hopefully function better on daily basis

--- touchpad_toggle.sh
+++ touchpad_toggle.sh
@@ -5,14 +5,17 @@ ID=`/usr/bin/xinput list | /usr/bin/grep -Eio 'touchpad\s*id\=[0-9]{1,2}' | /usr
 declare -i STATE
 STATE=`/usr/bin/xinput list-props $ID | /usr/bin/grep 'Device Enabled' | /usr/bin/awk '{print $4}'`
 
-if [ $STATE -eq 1 ]
+if [ $STATE -eq 0 ]
 then
-    /usr/bin/touch /tmp/touchpad_toggled.pid && /usr/bin/echo "1" > /tmp/touchpad_toggled.lock
-    /usr/bin/xinput disable $ID
-    /usr/bin/notify-send -i /usr/share/icons/touchpad/touchpad_disabled.svg 'Touchpad' 'Disabled' && sleep 3 &
-else
     /usr/bin/rm -f /tmp/touchpad_toggled.*
     /usr/bin/xinput enable $ID
     /usr/bin/notify-send -i /usr/share/icons/touchpad/touchpad_enabled.svg 'Touchpad' 'Enabled' && sleep 3 &
+else
+    /usr/bin/rm -f /tmp/touchpad_toggled.*
+    /usr/bin/xinput enable $ID
+    /usr/bin/touch /tmp/touchpad_toggled.pid && /usr/bin/echo "1" > /tmp/touchpad_toggled.lock
+    /usr/bin/chown srsantos:srsantos /tmp/touchpad_toggled.*
+    /usr/bin/xinput disable $ID
+    /usr/bin/notify-send -i /usr/share/icons/touchpad/touchpad_disabled.svg 'Touchpad' 'Disabled' && sleep 3 &
 fi
 
