fad7ed632f69f807eee2940cb96c011bdc178bad
Scott R. Santos <santos@auburn.edu>
2016-08-28 21:30:53 -0500

    Removed su since not needed to trigger xset/dpms

--- yubikey_removal_triggers_lock.sh
+++ yubikey_removal_triggers_lock.sh
@@ -5,7 +5,7 @@ export XAUTHORITY=/home/srsantos/.Xauthority
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
 if [ -z "$(lsusb | grep Yubikey)" ] ; then
-        /bin/su srsantos -c /usr/bin/xset dpms force off
+        /usr/bin/xset dpms force off
 fi
 
 exit
