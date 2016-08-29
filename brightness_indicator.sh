385c227df2fa1c04ad78b64b89c4843d6aceaaaa
Scott R. Santos <santos@auburn.edu>
2016-08-29 12:40:26 -0500

    Added new progress bar indicators for brightness and volume

---
+++ brightness_indicator.sh
@@ -0,0 +1,35 @@
+#!/bin/env sh
+
+usage="usage: $0 -c {up|down} -i [increment]"
+command=
+increment=15
+
+while getopts i:h o
+do case "$o" in
+    i) increment=$OPTARG;;
+    h) echo "$usage"; exit 0;;
+    ?) echo "$usage"; exit 0;;
+esac
+done
+
+shift $(($OPTIND - 1))
+command=$1
+
+if [ "$command" = "" ]; then
+    echo "usage: $0 {up|down} [increment]"
+    exit 0;
+fi
+
+display_brightness=0
+
+if [ "$command" = "up" ]; then
+    display_brightness=$(xbacklight -inc $increment && xbacklight -get | awk -F"." '{print $1}')
+    icon_name="/usr/share/icons/gnome-colors-common/scalable/notifications/notification-display-brightness-high.svg"
+fi
+
+if [ "$command" = "down" ]; then
+    display_brightness=$(xbacklight -dec $increment && xbacklight -get | awk -F"." '{print $1}')
+    icon_name="/usr/share/icons/gnome-colors-common/scalable/notifications/notification-display-brightness-low.svg"
+fi
+
+notify-send " " -i $icon_name -h int:value:$display_brightness -h string:synchronous:brightness --expire-time=100
