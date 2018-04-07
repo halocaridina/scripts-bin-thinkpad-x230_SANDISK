ec7d4d4603f846d2eef1791c58a143fe74b681c8
Scott R. Santos <santos@auburn.edu>
2018-04-06 21:13:19 -0500

    Various tweaks

--- rofi_i3exit.sh
+++ rofi_i3exit.sh
@@ -1,12 +1,12 @@
  #!/usr/bin/env bash
 
-res=$(rofi -dmenu -lines 6 -columns 3 -u 2-6 -font "xos4 Terminus 16" -p "What do you want to do? " -input < ~/.i3/rofi-i3exit.opts)
+res=$(rofi -dmenu -lines 6 -columns 3 -u 2-5 -font "xos4 Terminus 16" -p "What do you want to do? " -input < ~/.i3/rofi-i3exit.opts)
 
 if [[ $res = "LOCK SCREEN" ]]; then
     xset dpms force off
 fi
 if [[ $res = "LOGOUT TO TTY" ]]; then
-    pkill -f xss-lock && pkill -f bash && pkill -f gpg-agent && rm -f /tmp/screen_lock_toggled_off.lock /tmp/openconnect_vpn.lock && i3-msg exit
+    pkill -f xss-lock & pkill -f bash & pkill -f gpg-agent & rm -f /tmp/screen_lock_toggled_off.lock /tmp/openconnect_vpn.lock & i3-msg exit
 fi
 if [[ $res = "SUSPEND TO RAM" ]]; then
     systemctl suspend
