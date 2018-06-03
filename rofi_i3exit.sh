94bb4829abfb62d50266ff0bf000452815ff16e4
Scott R. Santos <santos@auburn.edu>
2018-06-02 23:18:24 -0500

    Increased font size

--- rofi_i3exit.sh
+++ rofi_i3exit.sh
@@ -1,6 +1,6 @@
  #!/usr/bin/env bash
 
-res=$(rofi -dmenu -lines 6 -columns 3 -u 2-5 -font "xos4 Terminus 18" -p " " -input < ~/.i3/rofi-i3exit.opts)
+res=$(rofi -dmenu -lines 6 -columns 3 -u 2-5 -font "xos4 Terminus 20" -p " " -input < ~/.i3/rofi-i3exit.opts)
 
 if [[ $res = "LOCK SCREEN" ]]; then
     xset dpms force off
