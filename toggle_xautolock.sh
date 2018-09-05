cceff83eb475637504af36301b8ff0e80a4e769c
Scott R. Santos <santos@auburn.edu>
2018-09-04 22:11:04 -0500

    Tweaked wording

--- toggle_xautolock.sh
+++ toggle_xautolock.sh
@@ -5,10 +5,10 @@ TOGGLE="/tmp/screen_lock_toggled_off.lock"
 if [ ! -e $TOGGLE ]; then
     touch $TOGGLE && echo "1" > /tmp/screen_lock_toggled_off.lock
     /usr/bin/pkill xautolock
-    notify-send 'Auto-Suspend' 'Toggled' -i /usr/share/icons/gnome/48x48/status/changes-allow.png
+    notify-send 'Auto-Suspend' 'Off' -i /usr/share/icons/gnome/48x48/status/changes-allow.png
 else
     rm -f $TOGGLE
     /usr/bin/xautolock -time 30 -locker "systemctl suspend" -detectsleep &
-    notify-send 'Auto-Suspend' 'Toggled' -i /usr/share/icons/gnome/48x48/status/changes-prevent.png
+    notify-send 'Auto-Suspend' 'On' -i /usr/share/icons/gnome/48x48/status/changes-prevent.png
 fi
 
