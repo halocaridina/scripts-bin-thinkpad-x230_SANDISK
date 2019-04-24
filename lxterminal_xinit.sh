2de5f8bab0d4446798b8c9deb8b18f6ff7610394
Scott R. Santos <santos@auburn.edu>
2019-04-23 23:47:44 -0500

    Added lxterminal for window managerless session

---
+++ lxterminal_xinit.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/usr/bin/lxterminal --geometry=1366x768 -e "tmux new -s from_xinit"
--- termite_xinit.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/termite --geometry=1366x768 -e "tmux new -s from_xinit"
